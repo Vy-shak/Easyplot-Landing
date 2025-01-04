69033ee99228629da3716c4a2e8d20358441c960
yshak <Vyshakn29@gmail.com>
2025-01-04 14:48:04 +0530

    cleaning

--- app/customComp/index.ts
+++ app/customComp/index.ts
@@ -1,7 +1,4 @@
-import Navbar from "./Navbar";
-import DottedBackground from "./Dottedbg";
-import Alertbox from "./Errorbox/Alertbox";
-import InfoCard from "./card/InfoCard";
+
 import NavbarL from "./Landing/NavbarL";
 
 
@@ -11,4 +8,4 @@ import NavbarL from "./Landing/NavbarL";
 
 
 
-export { Navbar, DottedBackground, Alertbox, InfoCard, NavbarL }
+export { NavbarL }
